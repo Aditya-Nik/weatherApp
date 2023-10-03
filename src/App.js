b9161cedb68a72be90faa013e070c0c31e41d45b
Aditya-Nik <adigkp2024@gmail.com>
2023-10-03 14:17:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// when we want to use restaurant app
+// import Restaurant from "./component/basics/Restaurant"; 
+
+// when you want to use todo app
+// import Todo from "./component/todoreact/todo";
+
+// when you want to use weather app
+import Temp from "./component/weather/temp";
+const App = () => {
+  // return <Restaurant />
+  // return(<><Todo /></>)
+
+  return (
+    <>
+      < Temp />
+    </>
+  )
+}
+
+export default App
